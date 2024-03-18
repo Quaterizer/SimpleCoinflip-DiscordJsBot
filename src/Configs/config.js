d7065d5c16c2ff823ca8ae2fcb3243be658f1318
Rise <83676023+Quaterizer@users.noreply.github.com>
2024-03-18 09:39:41 +0700

    Update config.js

--- src/Configs/config.js
+++ src/Configs/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-  GUILD_ID: "",
-  BOT_ID: "",
-  TOKEN: "",
+  GUILD_ID: "Your Server Id",
+  BOT_ID: "Your Client Bot",
+  TOKEN: "You Token Bot",
 };
